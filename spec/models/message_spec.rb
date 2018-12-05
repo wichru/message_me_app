4c3b1eb3f00f11dca44ecae295b8b17eeff2ec22
wichru <wicher.maciej@gmail.com>
2018-12-05 22:05:00 +0100

    Rspec validation for message model with and without shoulda-matchers

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:body) }
+
+  describe 'validations' do
+    let(:user) { User.create(username: 'John', password: '0ksymoron1') }
+    let(:message) { Message.create(body: body, user: user) }
+
+    subject { message.valid? }
+
+    context 'when it has not have body' do
+      let(:body) { nil }
+
+      it "it's invalid" do
+        is_expected.to eq false
+      end
+    end
+    context 'when it has body' do
+      let(:body) { 'This is a test message' }
+
+      it "it's valid" do
+        is_expected.to eq true
+      end
+    end
+  end
 end

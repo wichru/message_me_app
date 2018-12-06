d584c74939d16b885843c4f35f691a1f706a308d
wichru <wicher.maciej@gmail.com>
2018-12-06 23:59:43 +0100

    Render view for index page

--- spec/views/chatroom/index.html.erb_spec.rb
+++ spec/views/chatroom/index.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "chatroom/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'chatroom/index.html.erb', type: :view do
+  it 'renders form' do
+    assign(:message, Message.new)
+    assign(:message, [])
+
+    render
+
+    expect(rendered).to match(messages)
+  end
 end

0feb17c01d814aa51affb45d22f536b76cf08606
wichru <wicher.maciej@gmail.com>
2018-12-06 23:58:56 +0100

    Secured from unassigned users

--- spec/controllers/chatroom_controller_spec.rb
+++ spec/controllers/chatroom_controller_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ChatroomController, type: :controller do
+  describe 'not signed user' do
+    it 'redirects to login path' do
+      get :index
 
-  describe "GET #index" do
-    it "returns http success" do
+      expect(response).to redirect_to(controller: 'sessions',
+                                      action: 'new')
+    end
+  end
+
+  describe 'signed user' do
+    let(:user) { User.new(username: 'John', password: 'Bonjovi') }
+
+    it 'returns http success' do
+      log_in user
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end

27d5a89a29ff9461bfc48fc75a68687efae38ca3
wichru <wicher.maciej@gmail.com>
2018-12-06 23:57:05 +0100

    Allow to create messages fo logged users in specs

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
+  describe 'signed user' do
+    before do
+      user = User.create(username: 'John', password: 'test123')
+    end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    describe 'GET #create' do
+      it 'create message' do
+        expect do
+          post :create, params: { message: { body: 'Please work!' } }
+        end.to change(Message, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
-
 end

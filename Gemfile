b32b880edcc5efeaece5b64bc43f13d07943cb28
wichru <wicher.maciej@gmail.com>
2018-12-07 23:41:28 +0100

    Create session spec for create action and simplecov gem added

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jquery-rails'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'hirb'
+gem 'simplecov', require: false, group: :test
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'POST #create' do
+    context 'when password is correct' do
+      let(:user) { User.create(username: 'John', password: 'test123') }
+      it 'redirect to root path' do
+        post :create, session: { username: 'John', password: 'test123' }
+
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context 'when password is incorrect' do
+      let(:user) { User.create(username: 'John', password: 'test123') }
+
+      it 'set the error message and render new' do
+        post :create, session: { username: 'John', password: '' }
+
+        expect(response).to have_http_status(:render)
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end

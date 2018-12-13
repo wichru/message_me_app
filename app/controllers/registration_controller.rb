ab4dfde85136cd632d8908481a5203af591e6d1c
wichru <wicher.maciej@gmail.com>
2018-12-13 01:48:29 +0100

    Add missing specs plus fixed registration issue

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,19 +4,19 @@ class RegistrationController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user ||= User.new(user_params)
 
-    if user.save
-      session[:current_user] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render 'new', status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username, :password)
   end
 end
--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -15,11 +15,22 @@ RSpec.describe RegistrationController, type: :controller do
         { user: { username: 'John', password: 'test123' } }
       end
 
+      let(:invalid_params) do
+        { user: { username: 'fo', password: 'test123' } }
+      end
+
       it 'creates user' do
         expect do
           post :create, params: valid_params
         end.to change(User, :count).by(1)
-        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'checks user length' do
+        expect do
+          post :create, params: invalid_params
+        end.not_to change(User, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
